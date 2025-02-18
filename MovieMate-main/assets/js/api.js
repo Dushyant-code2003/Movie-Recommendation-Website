d803f6e3b227586f540965efb43bebf188c9cd27
Dushyant-code2003 <dushyant.23cse@bmu.edu.in>
2025-02-19 03:10:42 +0530

    Update api.js

--- MovieMate-main/assets/js/api.js
+++ MovieMate-main/assets/js/api.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const api_key = "2987b1f10f37fa81514dba47952ef70d";
+const api_key = "";
 const imageBaseURL = "https://image.tmdb.org/t/p/";
 
 // Fetch data from a server using the 'url' and pass the result in JSON data to a 'callback' function along with an optional parameter if has 'optionalParam'
